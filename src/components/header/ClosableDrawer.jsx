b469dafa76c5d5d82f818a3c643c257fb1a345b6
Ararchy01 <ararchy0621@gmail.com>
2021-01-15 23:32:04 -0800

    Added drawer

---
+++ src/components/header/ClosableDrawer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/styles";
+import IconButton from "@material-ui/core/IconButton";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import HistoryIcon from "@material-ui/icons/History";
+import PersonIcon from "@material-ui/icons/Person";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+const ClosableDrawer = (props) => {};
+
+export default ClosableDrawer;
